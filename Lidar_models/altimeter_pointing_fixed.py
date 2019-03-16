eccf62167c942eadecf298e40e576ae99b2d79c1
Brian Gaudet <briangaudet@mac.com>
2019-03-16 08:46:05 -0700

    Add files via upload

---
+++ Lidar_models/altimeter_pointing_fixed.py
@@ -0,0 +1,67 @@
+
+
+import numpy as np
+import attitude_utils as attu
+
+class Altimeter(object):
+    def __init__(self, measurement_model, target_position,  dphi=0.0, theta=np.pi/8, nbeams=4,  attitude_parameterization=attu.Quaternion_attitude()):
+        self.measurement_model = measurement_model
+        self.target_position = target_position
+        self.theta = theta
+        self.nbeams = nbeams
+        self.attitude_parameterization = attitude_parameterization
+        self.refdvec_b = np.asarray([0.0, 0.0, -1.0])
+        self.phis = np.linspace(-np.pi,np.pi-2*np.pi/nbeams,nbeams) 
+        self.dphi = dphi
+        self.miss_cnt = 0
+        self.cnt = 0
+ 
+        self.dvecs_b = self.get_body_frame_attitude()
+        #print(self.dvecs_b)
+ 
+    def get_body_frame_attitude(self):
+        self.phis += self.dphi 
+        self.phis = attu.vec_picheck(self.phis)
+        dvecs_b = []
+        for i in range(self.phis.shape[0]):
+            dvecs_b.append(-np.asarray([np.sin(self.theta)*np.cos(self.phis[i]),
+                                     np.sin(self.theta)*np.sin(self.phis[i]),
+                                     np.cos(self.theta)]))
+        
+        return  np.asarray(dvecs_b)
+        
+
+    def get_inertial_dvecs(self, dvecs_b, position, velocity):
+        dvec_1 = velocity / (np.linalg.norm(velocity) + 1e-8)
+        dvec_i = -position / np.linalg.norm(position) 
+        dvec_i /= np.linalg.norm(dvec_i)
+        C = attu.DCM(self.refdvec_b, dvec_i)
+        dvecs_i = C.dot(dvecs_b.T).T
+
+        return dvecs_i
+
+    def get_reading(self, position, velocity):
+        dtm_position = position + self.target_position
+
+        dvecs_i = self.get_inertial_dvecs(self.dvecs_b, position, velocity)
+        altitudes = [] 
+        cvs = []
+        vtests = []
+
+        for dv in dvecs_i:
+            #print('DV: ', dv) 
+            a, v, v_test = self.measurement_model.get_range(dtm_position, velocity, dv)
+            altitudes.append(a)  # was a.copy()
+            cvs.append(v) 
+            vtests.append(v_test)
+        miss = np.all(v_test is None)
+        if miss:
+            self.miss_cnt += 1
+        self.cnt += 1
+        if self.cnt % 10000 == 0:
+            print('Miss Ratio: ', self.miss_cnt / self.cnt)
+ 
+        return np.asarray(altitudes), np.asarray(cvs)
+         
+        
+        
---
+++ Lidar_models/lidar_measurement_model.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class Lidar_measurement_model(object):
+
+    """
+    position should be in asteroid centered reference frame
+    Asteroid represented as a point cloud of vertices, but cut in half to avoid spurious readings
+
+    """
+    def __init__(self, shape_model, max_range=10000):
+        indices = np.where(shape_model[:,2] > 0)[0]
+        shape_model = shape_model[indices]
+        self.shape_model = shape_model
+        self.max_range = max_range
+        print('Half Shape Model: ', np.min(shape_model,axis=0), np.max(shape_model,axis=0), shape_model.shape[0])
+        m = shape_model.shape[0]
+        D = []
+        for i in range(m):
+            v = shape_model[i]
+            sm_f = shape_model[np.arange(m)!=i] 
+            d = np.min(np.linalg.norm(sm_f-v,axis=1))
+            D.append(d)
+        print('Shape Model Vertex Spacing (mean / min / max): ', np.mean(D),np.min(D), np.max(D))
+        self.miss_threshold = 2 *  np.max(D)
+
+    # asteroid body-centered frame
+ 
+    def get_range(self, pos, vel, dvec):
+        test_dot = np.dot(-pos/np.linalg.norm(pos) , dvec)
+        if test_dot < 0:
+            return self.max_range, 0.0, None
+
+        closest, dist = self.closest_vertex(self.shape_model, pos, dvec)
+        if dist > self.miss_threshold:
+            return self.max_range, 0.0, None
+
+        range = np.linalg.norm(closest - pos)
+        dopplar = -pos.dot(vel)/np.linalg.norm(pos-closest)
+        #print('*closest: ', closest)
+        #print('pos/vel: ', pos,vel)
+        #print('range: ', range)
+        #print('dopplar: ', dopplar)
+        return range, dopplar, closest
+
+    def ortho_proj(self, V,p,d):
+        # vertices, position, dvec
+        Q=V-p
+        qdot = Q.dot(d)
+        proj = (qdot * np.expand_dims(d, axis=1)).T
+        oproj = Q-proj
+        return oproj
+
+    def closest_vertex(self, V,p,d):
+        dist = np.linalg.norm(self.ortho_proj(V,p,d),axis=1)
+        idx = np.argmin(dist)
+        return V[idx], dist[idx]
